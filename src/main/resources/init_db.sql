3c8802c7db97db6d6d3a7331f93e12254f0b6237
Vaniaina <hei.vaniaina@gmail.com>
2025-12-05 11:31:22 +0300

    ADD: init_db

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,15 @@
+-- Creation de base de données
+CREATE DATABASE product_management_db;
+
+-- Creation de l'utilisatuer
+CREATE USER product_manager_user WITH PASSWORD '123456';
+
+--  Accorder le droit de se connecter à la DB
+GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
+\c product_management_db;
+
+-- Accorder le droit de creer des tables dans la DB
+GRANT CREATE ON DATABASE product_management_db TO product_manager_user;
+
+-- Accorder le droit de faire le CRUD
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO product_manager_user;
