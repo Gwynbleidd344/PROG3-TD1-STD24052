1c54e9c98b50f7e13be49aa35c5d52f893961042
Vaniaina <hei.vaniaina@gmail.com>
2025-12-07 15:50:11 +0300

    feat: added DBConnection details

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private final String JDBC_URL = "jdbc:postgresql://localhost:5432/your_database_name";
-    private final String USER = "your_username";
-    private final String PASSWORD = "your_password";
+    private final String JDBC_URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private final String USER = "postgres";
+    private final String PASSWORD = "2048";
 
     public Connection getDBConnection() {
         Connection connection = null;
